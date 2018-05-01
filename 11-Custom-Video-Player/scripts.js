383c15304a6bb25f0481acada846a51beb2d9382
Nick Arocho <nick@boldlyfine.com>
2018-05-01 14:43:49 -0700

    Add scrub feature

--- 11-Custom-Video-Player/scripts.js
+++ 11-Custom-Video-Player/scripts.js
@@ -24,22 +24,39 @@ function togglePlay() {
 function updateButton() {
   const icon = this.paused ? '►' : '| |';
   toggle.textContent = icon;
-}
+};
 
 function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
-}
+};
 
 function handleRangeUpdate() {
   video[this.name] = this.value;
-}
+};
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`
+};
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+};
 
 /* Hook Up Our Event Listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+progress.addEventListener('click', scrub);
+let mousedown = false; // flag to tell whether the user is clicking down for scrub to happen
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e)); // mousedown && scrub(e) is a shortcut/short circuit for an if statement
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
